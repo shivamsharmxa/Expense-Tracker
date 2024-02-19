ce66f2317e68a9b325a9dd6213a03af8bf2211b0
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-02-19 13:32:18 +0530

    fix show and hide button

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import Sign from "./components/authentication/Sign";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Corrected import
 import './App.css';
 
+
 function App() {
   return (
-    <div className="app">
+   
     <Router> {/* Use Router instead of Routes here */}
       <Routes>
         <Route path='/' element={<Login />} />
         <Route path='/signup' element={<Sign />} />
+       
         {/* Chat index page */}
       </Routes>
     </Router>
-    </div>
+    
   );
 }
 
--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   FormControl,
   FormLabel,
@@ -12,44 +12,54 @@ import {
 } from '@chakra-ui/react';
 
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
   return (
+    <div className='app'>
     <Box
       position="absolute"
-      top="50%"
+      top="48%"
       left="81%"
       transform="translate(-50%, -50%)"
-      backgroundColor="transparent"
+      backgroundColor="rgb(66 153 225 / 6%);"
       p="4"
       borderRadius="md" // Add border radius for styling
       boxShadow="md" // Add a box shadow for styling
     >
       <VStack spacing="5px" color="black" width="300px">
-        <Text fontSize="xl" fontWeight="bold" mb="4">
+        <Text fontSize="5xl" fontWeight="bold" mb="4">
           Login
         </Text>
 
         <FormControl id="email" isRequired>
           <FormLabel>Email</FormLabel>
-          <Input placeholder="Enter Your Email" />
+          <Input placeholder="Enter Your Email" color={"white"} />
         </FormControl>
 
         <FormControl id="password" isRequired>
-          <FormLabel>Password</FormLabel>
-          <InputGroup size="md">
-            <Input type="password" placeholder="Enter Password" />
-            <InputRightElement width="4.5rem">
-              <Button h="1.75rem" size="sm">
-                Show
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-        </FormControl>
+      <FormLabel>Password</FormLabel>
+      <InputGroup size="md">
+        <Input
+          type={showPassword ? 'text' : 'password'}
+          placeholder="Enter Password"
+          color={"white"}
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" onClick={handleTogglePassword}>
+            {showPassword ? 'Hide' : 'Show'}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    </FormControl>
 
         <Button colorScheme="blue" width="100%" style={{ marginTop: 15 }}>
           Log In
         </Button>
       </VStack>
     </Box>
+    </div>
   );
 };
 
