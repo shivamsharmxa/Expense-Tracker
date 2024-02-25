d2cb04e54646241c1d52fb7555170f33befb28a1
Shivam Sharma <136487437+shivamsharmxa@users.noreply.github.com>
2024-02-25 14:05:06 +0530

    merge changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
-import Login from "./components/authentication/Login";
-import Sign from "./components/authentication/Sign";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Corrected import
 import './App.css';
+import Graph from "./components/authentication/charts/Graph";
+import Form from "./components/authentication/charts/Form";
+
+
 
 
 function App() {
   return (
-   
-    <Router> {/* Use Router instead of Routes here */}
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/signup' element={<Sign />} />
-       
-        {/* Chat index page */}
-      </Routes>
-    </Router>
-    
+  <div className="App">
+    <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+      <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
+
+      {/* grid columns */}
+      <div className="grid md:grid-cols-2 gap-4">
+          {/* Chart */}
+          <Graph></Graph>
+          {/* Form */}
+          <Form/>
+      </div>
+    </div>
+  </div>
   );
 }
 
 export default App;
-
-
